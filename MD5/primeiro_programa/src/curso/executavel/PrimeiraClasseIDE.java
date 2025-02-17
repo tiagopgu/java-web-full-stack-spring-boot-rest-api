dfae93988a903e32124877c11c6ee9a52ca8ab6f
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2025-02-17 20:45:51 -0300

    Projetos criados para teste de IDE

---
+++ MD5/primeiro_programa/src/curso/executavel/PrimeiraClasseIDE.java
@@ -0,0 +1,10 @@
+package curso.executavel;
+
+public class PrimeiraClasseIDE {
+
+	/* Ponto de entrada da aplicação */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!"); // Escreve no console
+		
+	}
+}
