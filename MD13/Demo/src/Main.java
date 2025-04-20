49690a7c5ead4a4d8a0afb8fc0e88bafa3c3dcdc
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2025-04-20 09:09:54 -0300

    Exemplo de manipulação dinâmica de array.

--- MD13/Demo/src/Main.java
+++ MD13/Demo/src/Main.java
@@ -1,17 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nEstudos dos Arrays\n");
 
-        double[] notas = new double[4];
+        double[] notas = new double[getTamanhoArray()];
 
-        notas[0] = 8.6;
-        notas[1] = 7.3;
-        notas[2] = 9.9;
-        notas[3] = 6.9;
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = getNota(i + 1);
+        }
 
         System.out.println("Primeira nota: " + notas[0] + "\n");
 
         for (int i = 0; i < notas.length; i++)
             System.out.println("Nota " + (i + 1) + " = " + notas[i]);
+    }
+
+    private static int getTamanhoArray() {
+        Scanner console = new Scanner(System.in);
+        int valor = 0;
+        String msgErro = "Valor inválido: deve ser informado um número maior que 0.";
+
+        do {
+            System.out.print("Informe a quantidade de notas: ");
+
+            try {
+                valor = console.nextInt();
+
+                if (valor <= 0)
+                    System.out.println(msgErro + "\n");
+            } catch (Exception ex) {
+                System.out.println(msgErro + "\n");
+            }
+
+            console.nextLine();
+        } while (valor <= 0);
+
+        return valor;
+    }
+
+    private static double getNota(int pos) {
+        Scanner console = new Scanner(System.in);
+        double nota = -1;
+        String msgErro = "Nota inválida: deve estar entre 0 e 10";
+
+        do {
+            System.out.print("Informe a " + pos + "ª nota: ");
+
+            try {
+                nota = console.nextDouble();
+
+                if (nota < 0 || nota > 10)
+                    System.out.println(msgErro + "\n");
+            } catch (Exception ex) {
+                System.out.println(msgErro + "\n");
+            }
+
+            console.nextLine();
+        } while(nota < 0 || nota > 10);
+
+        return nota;
     }
 }
