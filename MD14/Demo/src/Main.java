7f927393e9cb549a8a0849e30cb5e1e94534dad7
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2025-07-07 08:30:40 -0300

    Simulando processamento demorado inserindo pausa no processamento de um código. Atualizando anotações sobre uso de recursos de pausa de uma thread.

--- MD14/Demo/src/Main.java
+++ MD14/Demo/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Iniciando o envio de e-mails...");
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Processando e-mail 'teste" + (i + 1) + "@teste.com.br'");
+
+            try {
+                Thread.sleep(1500);
+
+                System.out.println("\t> Iniciando envio do e-mail...");
+                Thread.sleep(1500);
+                System.out.println("\t> E-mail enviado.\n");
+
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("Finalizando o processamento de emails");
     }
 }
--- MD14/README.md
+++ MD14/README.md
@@ -3,3 +3,7 @@
 ###### Anotações referentes ao módulo 14
 
 Thread permite com que diversos processamentos ocorram em paralelo, sem travar o processo principal. Isso garante mais fluidez ao sistema, não travando o usuário em tarefas que demandam um tempo maior de processamento, liberando-o para realizar diversas tarefas.
+
+## Inserindo Interrupção via código
+
+Para inserir interrupção por um período na execução do código, use `Thread.sleep(tempo_millisegundos)`. `Thread` pertence ao pacote `java.lang`  
