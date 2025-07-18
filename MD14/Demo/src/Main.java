4b5fae1be4f51ea43f1bf139f6b8a50b277a87d8
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2025-07-18 12:11:51 -0300

    Exemplo de como ter mais de uma thread executando ao mesmo tempo.

--- MD14/Demo/src/Main.java
+++ MD14/Demo/src/Main.java
@@ -1,27 +1,32 @@
 import javax.swing.*;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
         new Thread(Main::enviarEmails).start();
 
+        new Thread(Main::processarNotaFiscal).start();
+
         JOptionPane.showMessageDialog(null, "Envio de email em andamento. Você pode continuar acessando outras áreas do sistema. Você será notificado quando o envio de email finalizar.");
     }
 
     private static void enviarEmails() {
-        System.out.println("Iniciando o envio de e-mails...");
+        System.out.println("Iniciando o envio de e-mails...\n");
+        String prefixServico = "[EMAIL]";
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Processando e-mail 'teste" + (i + 1) + "@teste.com.br'");
+            String emailEmProcessamento = "'teste" + (i + 1) + "@teste.com.br'";
+            System.out.println(prefixServico + " Processando e-mail " + emailEmProcessamento);
 
             try {
                 Thread.sleep(1500);
 
-                System.out.println("\t> Iniciando envio do e-mail...");
-                Thread.sleep(1500);
-                System.out.println("\t> E-mail enviado.\n");
+                System.out.println(prefixServico + " Iniciando envio do e-mail " + emailEmProcessamento);
+                Thread.sleep(1200);
+                System.out.println(prefixServico + " E-mail " + emailEmProcessamento + " enviado.");
 
-                Thread.sleep(1000);
+                Thread.sleep(800);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -30,5 +35,31 @@ public class Main {
         System.out.println("Finalizando o processamento de emails");
 
         JOptionPane.showMessageDialog(null, "Envio de e-mail finalizado.");
+    }
+
+    private static void processarNotaFiscal() {
+        System.out.println("Iniciando processamento das notas fiscais\n");
+        String prefixServico = "[NFe]";
+
+        for (int i = 0; i < 10; i++) {
+            String notaFiscalEmProcessamento = "NFe 000" + (i + 1);
+            System.out.println(prefixServico + " Processando " + notaFiscalEmProcessamento);
+
+            try {
+                System.out.println(prefixServico + " Lendo arquivo " + notaFiscalEmProcessamento + ".pdf");
+                Thread.sleep(5000);
+
+                System.out.println(prefixServico + " Processando arquivo " + notaFiscalEmProcessamento + ".pdf");
+                Thread.sleep(500);
+
+                System.out.println(prefixServico + " Arquivo " + notaFiscalEmProcessamento + ".pdf processado.");
+
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "Processamento de notas fiscais finalizado.");
     }
 }
